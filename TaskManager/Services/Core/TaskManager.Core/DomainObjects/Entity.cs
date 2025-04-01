6f9c1c8a27edc23d0a31921c286f35646c64cc14
Edson Galindo <edson@yanel.com.br>
2025-03-31 23:06:15 -0300

    Create the task manager solution and add the initial projects of API and the service domain

---
+++ TaskManager/Services/Core/TaskManager.Core/DomainObjects/Entity.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TaskManager.Core.DomainObjects
+{
+    /// <summary>
+    /// The solution entity base class
+    /// </summary>
+    public class Entity
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+    }
+}
---
+++ TaskManager/Services/Task/TaskManager.Task.Domain/Task.cs
@@ -0,0 +1,40 @@
+﻿using TaskManager.Core.DomainObjects;
+using static TaskManager.Core.Shared.Task.Domain.TaskStatus;
+
+namespace TaskManager.Task.Domain
+{
+    /// <summary>
+    /// Represents a task entity
+    /// </summary>
+    public class Task : Entity
+    {
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime? DueDate { get; set; }
+        public StatusEnum Status { get; set; } = StatusEnum.Pending;
+
+        /// <summary>
+        /// Marks the task as pending.
+        /// </summary>
+        public void MarkAsPending()
+        {
+            Status = StatusEnum.Pending;
+        }
+
+        /// <summary>
+        /// Marks the task as in progress.
+        /// </summary>
+        public void MarkAsInProgress()
+        {
+            Status = StatusEnum.InProgress;
+        }
+
+        /// <summary>
+        /// Marks the task as completed.
+        /// </summary>
+        public void MarkAsCompleted()
+        {
+            Status = StatusEnum.Completed;
+        }
+    }
+}
