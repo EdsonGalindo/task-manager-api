d62787156077a6f3fbda6a2364c0e3c257136150
Edson Galindo <edson@yanel.com.br>
2025-04-01 22:56:05 -0300

    Add the Task Application and the Task Items View Model

---
+++ TaskManager/Services/Task/TaskManager.Tasks.Application/ViewModels/TaskItemViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static TaskManager.Core.Shared.Task.Domain.TaskStatus;
+
+namespace TaskManager.Tasks.Application.ViewModels
+{
+    public class TaskItemViewModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo Título é obrigatório")]
+        [StringLength(100, ErrorMessage = "O campo Título deve ter no máximo {1} caracteres", MinimumLength = 3)]
+        public string Title { get; set; } = string.Empty;
+
+        [StringLength(500, ErrorMessage = "O campo Descrição deve ter no máximo {1} caracteres")]
+        public string? Description { get; set; }
+
+        public DateTime? DueDate { get; set; }
+
+        public StatusEnum Status { get; set; } = StatusEnum.Pending;
+    }
+}
