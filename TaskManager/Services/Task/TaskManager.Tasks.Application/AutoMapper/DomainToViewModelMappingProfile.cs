7721a504adc2a607c6696cfd82c282336fb465ff
Edson Galindo <edson@yanel.com.br>
2025-04-01 23:32:38 -0300

    Add the Application MVVM mappings

---
+++ TaskManager/Services/Task/TaskManager.Tasks.Application/AutoMapper/DomainToViewModelMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using TaskManager.Tasks.Application.ViewModels;
+using TaskManager.Tasks.Domain;
+
+namespace TaskManager.Tasks.Application.AutoMapper
+{
+    public class DomainToViewModelMappingProfile : Profile
+    {
+        public DomainToViewModelMappingProfile()
+        {
+            CreateMap<TaskItem, TaskItemViewModel>();
+        }
+    }
+}
